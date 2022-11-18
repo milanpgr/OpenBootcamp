3092badfed54a7bccf231df0286d09125bf3124e
Milan <103267286+milanpgr@users.noreply.github.com>
2022-11-18 06:27:38 +0100

    Add files via upload

--- JavaBasico/0.2JavaSintaxis/EjercicioSesion3.java
+++ JavaBasico/0.2JavaSintaxis/EjercicioSesion3.java
@@ -1,21 +1,24 @@
 package OBSesion2;
+/*
+En este ejercicio tenéis que crear un bucle que permita concatenar textos e imprima el resultado final por consola.
 
+Tened en cuenta que los textos pueden venir de un array de tipo String. Por ejemplo:
+
+String[] nombres = {"", "", "", ""}
+ */
 public class EjercicioSesion3 {
 
     public static void main(String[] args) {
+        String nombre= "juan";
+        String nombre2="carlos";
+        String nombre3="gaviria";
+        String nombre4="lopez";
+        String[] nombres = new String[]{"juan"+ " carlos"+ " gaviria"+ " lopez"};
 
-        String cadena = new String("juan carlos lopez arboleda");
-        int nombres = 0;
-
-        int num = 1;
-        while (num <= nombres) {
-            cadena += "";
-            for (int i = 1; i <= num; i++ )
-                cadena += i;
-            num ++;
+        for(int i = 0; i < nombres.length; i++) {
+            System.out.println(nombres[i]);
 
         }
-        System.out.println(cadena);
-    }
 
+    }
 }
